6b4138f999f81f3176eee41071b867ff855471f9
JohannaGcd <guacidejohanna@gmail.com>
2025-02-03 16:10:58 +0100

    added lexer and parser folders

---
+++ include/minishell.h
@@ -0,0 +1,38 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h> // for testing!
+# include <stdlib.h>
+# include <unistd.h>
+
+// PART 1: DATA STRUCTURES
+
+// Lexical Analyser (ie. Lexer)
+
+// Parser
+
+typedef struct s_redirection
+{
+	char				*file;
+	int					fd;
+}						t_redirection;
+
+typedef struct s_command
+{
+	char				**command_args;
+	t_redirection		*in;
+	t_redirection		*out;
+	struct s_command	*next;
+}						t_command;
+
+// PART 2: FUNCTION DEFINITIONS
+
+// Lexical Analysis
+// -> Separates the input into tokens
+
+// Parsing
+// -> Processes the tokens according to a grammar and builds the command structs
+
+#endif
