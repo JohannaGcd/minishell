b52a0f42d55f631ce1052e12e9a28015e6bc1dc5
Svetlana Panfilova <spanfilo@f1r5s9.codam.nl>
2025-02-17 16:37:06 +0100

    env start and end

--- src/env/expansion.c
+++ src/env/expansion.c
@@ -34,31 +34,32 @@ void find_env(char *str, int *begin, int *end)
 int	expand_env(t_token *list_tokens, t_envs *envs)
 {
 	char *tmp;
-	int *begin_env;
-	int *end_env;
+	int begin_env;
+	int end_env;
 
 	while (list_tokens)
 	{
 		if (list_tokens->type == ENV)
 		{
-			{	
-				*begin_env = 0;
-				tmp = extract_env(list_tokens->str, envs);
-				if (tmp == NULL)
-				{
-					list_tokens->str = "";
-				}
-				else
-				{
+			find_env(list_tokens->str, &begin_env, &end_env);
+			//make var
+			tmp = extract_env(list_tokens->str, envs);
+			if (tmp == NULL)
+			{
+				list_tokens->str = "";
+			}
+			else
+			{
 				free(list_tokens->str);
 				list_tokens->str = tmp;
-				}
 			}
 			list_tokens->type = WORD;
 		}
 		if(list_tokens->type == D_QUOTE)
-		{
-
+		{	
+			begin_env = 0;
+			end_env = 0;
+			find_env(list_tokens->str, &begin_env, &end_env);
 		}
 		list_tokens = list_tokens->next;
 	}
