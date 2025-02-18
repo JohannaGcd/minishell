a9dea9708924cecd38e0dbfe445c13e22743dc59
Svetlana Panfilova <spanfilo@f1r5s17.codam.nl>
2025-02-18 15:22:10 +0100

    env broked

--- src/env/expansion.c
+++ src/env/expansion.c
@@ -22,11 +22,11 @@ void find_env(char *str, int *begin, int *end)
 			i++;
 			continue;
 		}
-		while (str[i] && (str[i] != '\"' || str[i] != '/'))
+		while (str[i] && str[i] != '\"' && str[i] != '/' && str[i] != ' ')
 		{
 			i++;
 		}
-		*end = i;
+		*end = i - 1;
 		break;
 	}
 }
@@ -35,6 +35,8 @@ int	expand_env(t_token *list_tokens, t_envs *envs)
 {
 	char *start;
 	char *mid;
+	char *st_and_mid;
+	char *end;
 	int begin_env;
 	int end_env;
 
@@ -45,6 +47,8 @@ int	expand_env(t_token *list_tokens, t_envs *envs)
 			begin_env = 0;
 			end_env = 0;
 			find_env(list_tokens->str, &begin_env, &end_env);
+			printf("begin_env=%d\n",begin_env);
+			printf("end_env=%d\n",end_env);
 			start = extract_env(list_tokens->str, begin_env, end_env, envs);
 			if (start == NULL)
 			{
@@ -52,8 +56,9 @@ int	expand_env(t_token *list_tokens, t_envs *envs)
 			}
 			else
 			{
+				end = ft_substr(list_tokens->str, end_env+1, ft_strlen(list_tokens->str)-end_env -1);
 				free(list_tokens->str);
-				list_tokens->str = ft_strjoin(start, (list_tokens->str)+end_env + 1);
+				list_tokens->str = ft_strjoin(start, end);
 				free(start);
 			}
 			list_tokens->type = WORD;
@@ -63,12 +68,22 @@ int	expand_env(t_token *list_tokens, t_envs *envs)
 			begin_env = 0;
 			end_env = 0;
 			find_env(list_tokens->str, &begin_env, &end_env);
+			printf("begin_env=%d\n",begin_env);
+			printf("end_env=%d\n",end_env);
 			if (begin_env > 0 && end_env > 0)
+			{
 			start = ft_substr(list_tokens->str, 0, begin_env);
+			printf("start=%s\n",start);
 			mid = extract_env(list_tokens->str, begin_env, end_env, envs);
-			start = ft_strjoin(start, mid);
+			printf("mid=%s\n",mid);
+			st_and_mid = ft_strjoin(start, mid);
+			printf("st_and_mid=%s\n",st_and_mid);
 			free(list_tokens->str);
-			list_tokens->str = ft_strjoin(start, (list_tokens->str)+end_env + 1);
+			list_tokens->str = ft_strjoin(st_and_mid, (list_tokens->str)+end_env + 1);
+			free(start);
+			free(mid);
+			free(st_and_mid);
+			}
 		}
 		list_tokens = list_tokens->next;
 	}
