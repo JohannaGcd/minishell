aebf04ca93e75cc8de4e612a78eff1ab536433d3
Svetlana Panfilova <sveta@svetlanas-air.home>
2025-02-24 21:13:05 +0100

    started executer

---
+++ include/executer.h
@@ -0,0 +1,19 @@
+#ifndef EXECUTER_H
+# define EXECUTER_H
+# include <termios.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <signal.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "env.h"
+
+
+
+#endif
