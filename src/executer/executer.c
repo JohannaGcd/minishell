222c0605c860d345da702b0b6baa97c604374229
JohannaGcd <guacidejohanna@gmail.com>
2025-04-12 14:33:45 +0200

    redudant exec_export and exec_export_print deleted. johanna-feature merge. todo: count_nbr_args breaks unexpectedly with QUOTE token + echo hello > output.txt outputs hello output.txt in the file output.txt intead of just hello because the command_args is not filled properly and adds the output.txt after the redirection as an argument of command_args

--- src/executer/executer.c
+++ src/executer/executer.c
@@ -6,7 +6,7 @@
 /*   By: sveta <sveta@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/06 13:52:31 by sveta         #+#    #+#                 */
-/*   Updated: 2025/04/12 14:23:36 by jguacide      ########   odam.nl         */
+/*   Updated: 2025/04/12 14:31:33 by jguacide      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,50 +37,6 @@ void	execute_single_command(t_command *command)
         // printf("in execute single command: redirect out is: %d with file %s\n", command->out->type, command->out->file);
         waitpid(pid, &status, 0);
     }
-}
-
-void exec_export_print(t_minishell *mshell)
-{
-    t_env_node	*node;
-
-    node = mshell->envs->env;
-    while (node)
-    {
-    printf("declare -x %s=\"%s\"\n",node->var, node->value);
-    node = node->next;
-    }
-}
-
-void exec_export(t_command *command, t_minishell *mshell)
-{
-    //add env
-    int count;
-    char *var;
-    char *value;
-    t_env_node	*node;
-
-    count = 0;
-    while(command->command_args[count])
-        count++;
-    if (count == 1)
-        exec_export_print(mshell);
-    else if (count == 2)
-    {
-        var = ft_substr(command->command_args[1], 0, ft_strchr(command->command_args[1], '=') - command->command_args[1]);
-		value = ft_substr(command->command_args[1],
-				(ft_strchr(command->command_args[1], '=') - command->command_args[1]) + 1, ft_strlen(command->command_args[1]));
-                node = create_new_env_node(var, value);
-                add_env_to_list(&(mshell->envs->env), node);
-    }
-     else if (count == 3)
-    {
-        var = ft_substr(command->command_args[1], 0, ft_strchr(command->command_args[1], '=') - command->command_args[1]);
-		value = ft_substr(command->command_args[2], 0, ft_strlen(command->command_args[2]));
-        node = create_new_env_node(var, value);
-        add_env_to_list(&(mshell->envs->env), node); 
-    }
-    
-    
 }
 
 void execute_commands(t_minishell *mshell) 
