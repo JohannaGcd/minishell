a7ca4fbe3fafa3fc2ab267f5a0a740d415beba2d
Svetlana Panfilova <sveta@mac-2.home>
2025-05-14 20:19:58 +0200

    added pwd and oldpwd

--- src/executer/execute_cd.c
+++ src/executer/execute_cd.c
@@ -6,7 +6,7 @@
 /*   By: sveta <sveta@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/11 21:46:54 by sveta         #+#    #+#                 */
-/*   Updated: 2025/05/04 13:01:00 by sveta         ########   odam.nl         */
+/*   Updated: 2025/05/14 19:53:24 by sveta         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,34 @@ void exec_cd(char **command_args, t_minishell *mshell)
     char *path;
     char cwd[PATH_MAX];
 
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        mshell->old_pwd =  ft_strdup(cwd);
+        if (find_env_var(mshell->envs, "PWD"))
+        if (find_env_var(mshell->envs, "OLDPWD"))
+            change_env_var(mshell->envs, "OLDPWD", cwd);
+    }   
     path = command_args[1];
     if (path == NULL)
     {
         path = get_env_var(mshell->envs, "HOME");
         if (path == NULL) {
-            perror("cd: HOME not set\n");
+            perror("cd: HOME not set");
             return;
         }
     }
     if (chdir(path) == 0) 
     {
         if (getcwd(cwd, sizeof(cwd)) != NULL)
-            change_env_var(mshell->envs, "PWD", cwd);   
+        {
+            if (find_env_var(mshell->envs, "PWD"))
+                change_env_var(mshell->envs, "PWD", cwd);
+            free(mshell->pwd);
+            mshell->pwd =  ft_strdup(cwd);
+        }   
         else 
             perror("getcwd failed");
     } 
-    else 
+    else  
         perror("cd failed");
 }
--- src/executer/execute_pwd.c
+++ src/executer/execute_pwd.c
@@ -6,7 +6,7 @@
 /*   By: sveta <sveta@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/04 12:11:03 by sveta         #+#    #+#                 */
-/*   Updated: 2025/05/04 12:52:47 by sveta         ########   odam.nl         */
+/*   Updated: 2025/05/14 20:15:45 by sveta         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 void	exec_pwd(char **command_args, t_minishell *mshell)
 {
-	char *pwd;
-
 	if(command_args[1])
 		perror("two many arguments for pwd command");
 	else
 	{
-		pwd = get_env_var(mshell->envs, "PWD");
-		if (pwd)
-			printf("%s",pwd);
+			printf("%s",mshell->pwd);
 	}
 }
