24d432a8c9167ae48306e988910e25ac490fa281
Svetlana Panfilova <storba@gmail.com>
2025-05-22 20:57:16 +0200

    fix ft_strjoin_with_char

--- libft/ft_strjoin_with_char.c
+++ libft/ft_strjoin_with_char.c
@@ -56,28 +56,15 @@ char	*ft_strjoin_with_char(char *s1, char *s2, char c)
 	char	*dest;
 	size_t	s1_len;
 	size_t	s2_len;
-	size_t	len;
-	size_t	i;
 
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	len = s1_len + s2_len + 2;
-	dest = (char *)malloc(sizeof(char) * len);
+	dest = (char *)malloc(sizeof(char) * (s1_len + s2_len + 2));
 	if (dest == NULL)
 		return (NULL);
-	i = 0;
-	while (i < s1_len)
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	dest[i] = c;
-	i = 0;
-	while (i < s2_len)
-	{
-		dest[s1_len + 1 + i] = s2[i];
-		i++;
-	}
-	dest[s1_len + i + 1] = '\0';
+	ft_memcpy(dest, s1, s1_len);
+	dest[s1_len] = c;
+	ft_memcpy(dest + s1_len + 1, s2, s2_len);
+	dest[s1_len + s2_len + 1] = '\0';
 	return (dest);
 }
